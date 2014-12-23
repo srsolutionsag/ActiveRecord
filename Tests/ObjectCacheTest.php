d166691126d7d0d109470f7a59d5866c4c15e4c6
Oskar Truffer <oskar.truffer@gmail.com>
2014-12-23 10:45:31 +0100

    failing test.

--- Tests/ObjectCacheTest.php
+++ Tests/ObjectCacheTest.php
@@ -36,6 +36,7 @@ class ObjectCacheTest extends PHPUnit_Framework_TestCase {
 
 	public function testTableExistant() {
 		$this->assertTrue($this->pdo->tableExists($this->table_name));
+		$this->assertTrue(false);
 	}
 
 
