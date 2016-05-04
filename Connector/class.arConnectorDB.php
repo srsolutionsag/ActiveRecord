51a759cb8a65ab14af188be8b8211aa64a712b60
Stefan Wanzenried <stefan.wanzenried@gmail.com>
2016-05-04 11:42:28 +0200

    Fix bug: Indexes were not created

--- Connector/class.arConnectorDB.php
+++ Connector/class.arConnectorDB.php
@@ -75,7 +75,7 @@ class arConnectorDB extends arConnector {
 			$existing_indices[] = $rec->column_name;
 		}
 		foreach ($arFieldList->getFields() as $i => $arField) {
-			if ($arField->getIndex() === 'true') {
+			if ($arField->getIndex() === true) {
 				if (!in_array($arField->getName(), $existing_indices)) {
 					$ilDB->addIndex($ar->getConnectorContainerName(), array( $arField->getName() ), 'i' . $i);
 				}
